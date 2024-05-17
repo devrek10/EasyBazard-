66fec3cd1e84071aca4b104fd71220454f1ea690
Mouhamed Lo <160361321+m3dev4@users.noreply.github.com>
2024-05-17 01:32:00 +0000

    Api Login & Register

--- easyBazard/frontend/src/main.jsx
+++ easyBazard/frontend/src/main.jsx
@@ -26,15 +26,20 @@ import { Route, RouterProvider, createRoutesFromElements } from 'react-router-do
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './redux/store.ts'
+import Login from './pages/auth/Login.jsx'
 
 
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<App />} />
+  <Route path='/' element={<App />}>
+     <Route path='/login' element={<Login />} />
+  </Route>
 ))
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </Provider>
 )
--- easyBazard/frontend/src/pages/auth/Navigation.jsx
+++ easyBazard/frontend/src/pages/auth/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import {
   AiOutlineHome,
@@ -8,17 +8,42 @@ import {
   AiOutlineShoppingCart,
   AiOutlineShopping,
   AiOutlineUserAdd,
+  AiOutlineLogin,
 } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
 import "./Navigation.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useLoginMutation } from "../../redux/api/usersSlice"; 
+import { logout } from "../../redux/features/auth/authSlice";
 
 const Navigation = () => {
+  const { userInfo } = useSelector((state) => state.auth)
   const [dropDownOpen, setDropdownOpen] = useState(false);
   const [sideBarMenu, setSideBarMenu] = useState(false);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropDownOpen);
   };
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [ logoutApiCall ] = useLoginMutation()
+
+  const handleLogout = async () => {
+    try {
+      await logoutApiCall().unwrap()
+      dispatch(logout())
+      navigate('/login')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  
+
+
+
   return (
     <div
       style={{ zIndex: 999 }}
@@ -49,7 +74,39 @@ const Navigation = () => {
           <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26} />
           <span className="hidden nav-item-name mt-[3rem] ">Carte</span>
         </Link>
+        <Link
+          to="/"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <FaHeart className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem] ">Favorite</span>
+        </Link>
       </div>
+
+      <div className="relative">
+        <button onClick={toggleDropdown}
+        className="flex items-center text-gray-800 focus:outline-none">
+         {userInfo?.name ? <span className="text-white">{userInfo.userName}</span> 
+         : <>
+         
+         </>}
+        </button>
+      </div>
+
+      <ul>
+        <li>
+          <Link to="/login" className="flex items-center transition-transform transform hover:translate-x-2">
+            <AiOutlineLogin className="mr-2 mt-[3rem]" size={26} />
+            <span className="hidden nav-item-name mt-[3rem] ">Se Connecter</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/register" className="flex items-center transition-transform transform hover:translate-x-2">
+            <AiOutlineUserAdd className="mr-2 mt-[3rem]" size={26} />
+            <span className="hidden nav-item-name mt-[3rem] ">S'inscrire</span>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
